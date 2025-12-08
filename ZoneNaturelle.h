d30e79ad97aff91173e45ce12f928276e68bf468
Diarra <diopdiarra285@gmail.com>
2025-12-08 16:08:27 +0100

    correction erreur dans ZU-ZN-constructible+metho const dans parcelle

---
+++ ZoneNaturelle.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//classes implémentées dans les autres fichiers
+#include "Parcelle.hpp"
+
+//la classe ZoneNaturelle hérite de la classe Parcelle
+class ZoneNaturelle:public Parcelle{
+
+};
